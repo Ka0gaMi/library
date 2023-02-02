2381f897fb15ebda6e34542d4ff77ba43d7b66ad
Ka0gaMi <121442495+Ka0gaMi@users.noreply.github.com>
2023-02-02 16:23:33 +0200

    Added book grid and a function to add books
    Added book grid that accepts new book. Also defined a Book object, which
    takes title, author, pages and whether or not the book was read. Next
    created a addBookToLibrary function which takes Book arguments and calls
    putBooksToGrid, which adds the book to the grid using DOM manipulation

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Button functionality //
+
 let deleteSentenceRunning = false;
 
 const addBtn = document.querySelector(".add-btn");
@@ -43,4 +45,49 @@ function deleteText() {
     }
   };
   deleteNextLetter();
+}
+
+// Book data structure //
+
+const bookGrid = document.getElementById("bookGrid");
+
+let myLibrary = [];
+
+function Book(title = "Unknown", author = "Unknown", pages = 0, read = false) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  putBooksToGrid();
+}
+
+function putBooksToGrid() {
+  bookGrid.innerHTML = "";
+
+  for (let i = 0; i < myLibrary.length; i++) {
+    const bookDiv = document.createElement("div");
+    bookDiv.classList.add("book-card");
+    bookGrid.appendChild(bookDiv);
+
+    const titleP = document.createElement("p");
+    titleP.textContent = "Title: " + myLibrary[i].title;
+    bookDiv.appendChild(titleP);
+
+    const authorP = document.createElement("p");
+    authorP.textContent = "Author: " + myLibrary[i].author;
+    bookDiv.appendChild(authorP);
+
+    const pagesP = document.createElement("p");
+    pagesP.textContent = "Pages: " + myLibrary[i].pages;
+    bookDiv.appendChild(pagesP);
+
+    const readP = document.createElement("p");
+    readP.textContent = "Read: " + myLibrary[i].read;
+    bookDiv.appendChild(readP);
+  }
 }
